eb9dfc500092245075546c880b936d831f7634ec
Besker Telisma <beskertelisma9@gmail.com>
2020-11-10 23:03:15 -0600

    updated js

---
+++ whipitup/src/FrontPage.js
@@ -0,0 +1,5 @@
+export default function FrontPage(){
+    return (
+        <h1>Front page is here</h1>
+    )
+}
---
+++ whipitup/src/Recipes.js
@@ -0,0 +1,5 @@
+export default function Recipes (){
+    return (
+        <h1>I am recipe</h1>
+    )
+}
