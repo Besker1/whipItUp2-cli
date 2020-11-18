14873c424d84a907b3ae85b558b5d9ce9176063d
Besker Telisma <beskertelisma9@gmail.com>
2020-11-18 14:31:03 -0600

    uodated css

--- whipitup/src/RecipesFolder/Recipes.js
+++ whipitup/src/RecipesFolder/Recipes.js
@@ -2,9 +2,11 @@ import React from 'react'
 export default function Recipes (props){
     const content = props.content.map(cont => <li>{cont}</li>)
     return (
-<div>
+<div className='container'>
+    <div className='recipe'>
     <h1>{props.title}</h1>
     <ul>{content}</ul>
+    </div>
  </div>
 
     )
