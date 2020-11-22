28e4fa4cc9a93eeb36beec27839820d6030d1a8f
Besker Telisma <beskertelisma9@gmail.com>
2020-11-21 23:22:12 -0600

    backend cleanup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,29 @@
 /* eslint-disable strict */
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV, errorHandler } = require('./config');
-const recipe_Router = require('./recipes/recipe_router');
-const logger = require('./logger');
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV, errorHandler } = require("./config");
+const recipe_Router = require("./recipes/recipe_router");
+const logger = require("./logger");
 
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 ///security and debugging
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-//for athorizations 
+// for athorizations
 // app.use(function validateBearerToken(req, res, next) {
-//   const {API_TOKEN} = process.env;
-//   const authToken = req.get('Authorization');
-// console.log(API_TOKEN, 'IAM',authToken)
+//   const { API_TOKEN } = process.env;
+//   const authToken = req.get("Authorization");
+//   console.log(API_TOKEN, "IAM", authToken);
 
-//   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+//   if (!authToken || authToken.split(" ")[1] !== API_TOKEN) {
 //     logger.error(`Unauthorized request to path: ${req.path}`);
-//     return res.status(401).json({ error: 'Unauthorized request' });
+//     return res.status(401).json({ error: "Unauthorized request" });
 //   }
 //   // move to the next middleware
 //   next();
@@ -34,7 +31,6 @@ app.use(cors());
 
 // calling the route to get the recipes upon request
 app.use(recipe_Router);
-
 
 // for error handling in case entry is not valid
 app.use(errorHandler);
