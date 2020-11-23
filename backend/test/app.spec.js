0449e5b0f7cb1a31a29d15a5cf48696d164448a5
Besker Telisma <beskertelisma9@gmail.com>
2020-11-23 16:34:50 -0600

    start testing

--- backend/test/app.spec.js
+++ backend/test/app.spec.js
@@ -1,11 +1,19 @@
-/* eslint-disable strict */
-const supertest = require('supertest');
-const app = require('../src/app');
+// /* eslint-disable strict */
+// const supertest = require('supertest');
+// const app = require('../src/app');
 
-describe('App', () => {
+describe("App", () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
-    supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!');
+    supertest(app).get("/").expect(200, "Hello, world!");
   });
+});
+
+describe("", () => {
+  let db;
+  let recipesTest = [
+    {
+      title: "Tofu",
+      content: ["great", "food", "good"],
+    },
+  ];
 });
